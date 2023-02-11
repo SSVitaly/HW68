fa2662c17f776ddf367d5662415bc7d7d8dbb112
SVitaly <user7_490@mail.ru>
2023-02-11 20:46:55 +0300

    решили задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Clear();
+
+Console.WriteLine("Вычислим функцию Аккермана A(m,n) для неотрицательных аргументов");
+Console.WriteLine("Введите аргумент m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите аргумент n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Ackermann(int m, int n)
+{
+if (m == 0)
+{
+    return n+1;
+}
+else
+{
+    if (n == 0)
+    {
+        return Ackermann((m-1), 1);
+    }
+    else
+    {
+        return Ackermann(m-1, Ackermann(m, (n-1)));
+    }
+}
+}
+Console.WriteLine($"Значение функции Аккермана A(m,n) для неотрицательных аргументов m = {m} и n = {n}: {Ackermann(m,n)}");
